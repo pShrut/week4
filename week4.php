ae955f5e587ccc89df010053d61359731c663e32
Shruti <sp2372@njit.edu>
2017-10-04 14:30:52 -0400

    commit.

--- week4.php
+++ week4.php
@@ -1,16 +1,37 @@
 <?php
+  $obj = new main();
 
-     $date = date ('Y-m-d', time ());
-     echo  "The value of \$date: ".$date."<br>";
-							    
+
+  class main   {
+
+     public function __construct() {
+     $date = date ('Y-m-d');
+     $display =  "The value of \$date: ".$date."<br>";
+     dateDisplay::printDate($display);
+   
+
      $tar = "2017/10/03";
      echo " The value of \$tar: ".$tar."<br>";
   
     $year = array("2012", "396", "300", "2000", "1100", "1089");
     echo "The value of \$year: ";
     print_r($year);
+    echo '<br>';
+     
 
-	
+   }
+
+
+   public function __destruct(){
+   echo '</br> done';
+ }   
+ }
+
+   
+ class dateDisplay {
+   static public function printdate($t){
+     echo $t;}
+     }
 
 ?>
 
